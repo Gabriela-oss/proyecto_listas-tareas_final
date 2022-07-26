51fb38de9a8f7c03611776acb36ae26e749ac95b
gabriela-oss <solisch.gabriela@gmail.com>
2022-07-26 16:43:37 -0600

    commit antes de actualizar angular

--- src/app/layout/form-tarea/form-tarea.component.ts
+++ src/app/layout/form-tarea/form-tarea.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, FormGroup, Validators, FormBuilder } from '@angular/forms';
 import { TareaService } from 'src/app/services/tarea.service';
 import { ListaService } from 'src/app/services/lista.service';
 import { Lista } from 'src/app/classes/lista';
@@ -12,14 +12,20 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
   styleUrls: ['./form-tarea.component.css']
 })
 export class FormTareaComponent implements OnInit {
+  urgencia:FormControl = new FormControl();
+  estados:FormControl = new FormControl();
 
   formTasks:FormGroup = new FormGroup({
     nombre:new FormControl("",[Validators.required]),
     descripcion: new FormControl("",[Validators.required]),
-    urgencia: new FormControl("",[Validators.required]),
-    estado: new FormControl("",[Validators.required]),
     datosContacto: new FormControl("",[Validators.required]), 
-    fechaVencimiento: new FormControl("", [Validators.required])
+    fechaVencimiento: new FormControl("", [Validators.required]), 
+    urgencia: this.fb.group({
+      urgencia:this.urgencia
+    }),
+    estado: this.fb.group({
+      estado:this.estados
+    })
   })
 
   // listas:Tarea[] 
@@ -27,7 +33,7 @@ export class FormTareaComponent implements OnInit {
   lista:Lista = new Lista("",0); 
   lists:Lista[]=[];
 
-  constructor(public ruta:ActivatedRoute, private t:TareaService, public ls:ListaService) { 
+  constructor(public ruta:ActivatedRoute, private t:TareaService, public ls:ListaService, private fb:FormBuilder ) { 
     // this.listas = ls.getLists();
 
     // ruta.paramMap.subscribe((p:ParamMap)=>{
@@ -47,16 +53,17 @@ export class FormTareaComponent implements OnInit {
       console.log(this.lists)
       // this.lists = data;
     });
+
+    this.formTasks.get("estados")?.valueChanges.subscribe((value)=>{console.log(value)})
   }
 
   addTask(){
     console.log(this.formTasks)
     if (this.formTasks.valid){
-      this.t.addTasks(this.lista, this.formTasks.get("nombre")?.value, this.formTasks.get("descripcion")?.value, this.formTasks.get("urgencia")?.value, this.formTasks.get("estado")?.value, this.formTasks.get("datosContacto")?.value, this.formTasks.get("fechaVencimiento")?.value)
+      this.t.addTasks(this.lista, this.formTasks.get("nombre")?.value, this.formTasks.get("descripcion")?.value, this.formTasks.get("urgencia")?.value, this.formTasks.get("estados")?.value, this.formTasks.get("datosContacto")?.value, this.formTasks.get("fechaVencimiento")?.value) 
       console.log("se guardo bien")
     }else{
       console.log("no se pudo agregar una tarea")
     }
   }
-
 }
